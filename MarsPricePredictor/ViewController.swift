cb233d1d94bfa9859105d7044c08d1d7865debab
Devan K <dkondody@perleybrook.com>
2018-04-23 10:58:17 +0530

    Initial Commit

---
+++ MarsPricePredictor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarsPricePredictor
+//
+//  Created by Devan on 23/04/18.
+//  Copyright © 2018 Devan K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
